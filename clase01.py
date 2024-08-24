04695bf94b1d8840f66de95ca4556ad1eb9cb8f8
César <114108300+CesarCOt@users.noreply.github.com>
2024-08-24 14:59:57 -0600

    Add files via upload

---
+++ clase01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+# Comentarios dentro del programca con el simbolo gato.
+# La primera línea se llama shebang
+
+
+print("Tenemos en pantalla")
+
+# La forma de ejecutar en ventana de comandos: python3 nombrearchivo.py
+
+nombre = "César"
+print("Nombre " + nombre)
+
+# No ejecuta variables de diferentes tipos.
+
+Mi_variable =  "num" + str(5)
+print(Mi_variable)
